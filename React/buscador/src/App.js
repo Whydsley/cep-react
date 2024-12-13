752a301972ac213ab368de60cf845435ed05395b
whyd mello <whyd.mello@hotmail.com>
2024-12-13 16:01:58 -0300

    feat:cep

---
+++ React/buscador/src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite seu CEP..."
+        />
+
+
+        <button className="buttonSearch">
+          
+        </button>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
