5b60d9c62b151d56543b5e1f62767bfc4790cc83
Whyd Mello <62163422+Whydsley@users.noreply.github.com>
2024-12-13 15:59:37 -0300

    Delete React/cv directory

--- React/cv/src/components/Layout/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { Content } from 'carbon-components-react/lib/components/UIShell';
-
-export const StyledContent = styled(Content)`
-  min-height: 100vh;
-`;
